29a508f6a460ac15c458e34482667e5bf7432bd1
Abu Adnaan <fattylee.remod@gmail.com>
2018-05-10 15:08:10 +0100

    finish implementing form display

---
+++ js/toggleForm.js
@@ -0,0 +1,29 @@
+window. addEventListener("load", function(){
+
+//display sign up form on page load
+const dataForm =document. querySelectorAll(".data-form");
+dataForm[0]. style. display ='block' ;
+
+const signup = document. getElementById("sign-up"),
+signin = document. getElementById("sign-in");
+ 
+signup.addEventListener("click", toggleForm);
+signin. addEventListener("click", toggleForm);
+
+//click events handler for sign-in and sign-up button
+function toggleForm(event){
+
+if(event. target. id ==="sign-in"){
+
+dataForm[0]. style. display = "none" ;
+dataForm[1]. style. display = "block" ;
+
+}else {
+
+dataForm[1]. style. display = "none" ;
+dataForm[0]. style. display = "block" ;
+}
+}//End toggleForm 
+
+
+});
