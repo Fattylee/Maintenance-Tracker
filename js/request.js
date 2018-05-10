6e4b2c498c033d504dd2bb3ff949235ac306eded
fattylee <fattylee.remod@gmail.com>
2018-05-10 20:41:14 +0100

    complete request-list.html file and all css is up to date un-need request.js file deleted

--- js/request.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("request-list").addEventListener("click",requestNavigator);
-
-//request list event handler (navigate to request-list.html)
-function requestNavigator(){
-    alert("hi");
-    //location.assign("request-list.html");
-}//End requestNavigator
