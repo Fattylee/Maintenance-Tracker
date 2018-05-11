44dfb5f4cf6dad67b3fb9e65aceb8c2def1d98d8
fattylee <fattylee.remod@gmail.com>
2018-05-11 17:14:35 +0100

    complete admin-disapprove.html markup

--- js/admin-request.js
+++ js/admin-request.js
@@ -7,7 +7,7 @@ for (let i = 1; i < 101; i++) {
    <li><span>Maintenance/Repair</span>: repair${i}</li>
    <li><span>Description</span>: fix problem${i}</li>
    <li><span>Date</span>: ${i}/${i}/197${i}</li>
-   <li><span>(Dis)approve</span>: ${(i%2===0) ? "approved":"disapproved"}</li>
+   <li><span>(Dis)approve</span>: <button class="success">approved</button> <button class="critical">disapproved</button></li>
    </ul>
    `; 
 }
