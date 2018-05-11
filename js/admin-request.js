fae692ae1bb4f74a2b5d528d36d5d4d5a8e18072
fattylee <fattylee.remod@gmail.com>
2018-05-11 17:35:30 +0100

    finish admin-resolve.html by updating admin-request.js file

--- js/admin-request.js
+++ js/admin-request.js
@@ -45,3 +45,18 @@ for (let i = 1; i < 31; i++) {
 }
 if(document.querySelector(".content-area-disapprove"))
 document.querySelector(".content-area-disapprove").innerHTML += markup;
+
+markup="";
+for (let i = 1; i < 46; i++) {
+   markup += `
+   <ul>
+   <li><span>Username</span>: yourname${i}</li>
+   <li><span>Maintenance/Repair</span>: repair${i}</li>
+   <li><span>Description</span>: fix problem${i}</li>
+   <li><span>Date</span>: ${i}/${i}/197${i}</li>
+   <li><span>Status</span>: <span class="success">resolved</span></li>
+   </ul>
+   `; 
+}
+if(document.querySelector(".content-area-resolve"))
+document.querySelector(".content-area-resolve").innerHTML += markup;
