b931c119c2d5cee3fe7d089e7b48da57ffe645cc
fattylee <fattylee.remod@gmail.com>
2018-05-11 16:56:08 +0100

    complete admin-approve.html page

--- js/admin-request.js
+++ js/admin-request.js
@@ -1,4 +1,4 @@
-const contentAreaDiv = document.querySelector(".content-area");
+
 let markup="";
 for (let i = 1; i < 101; i++) {
    markup += `
@@ -11,4 +11,22 @@ for (let i = 1; i < 101; i++) {
    </ul>
    `; 
 }
-contentAreaDiv.innerHTML += markup;
+if(document.querySelector(".content-area-all")){
+    document.querySelector(".content-area-all").innerHTML += markup;
+}
+
+
+markup="";
+for (let i = 1; i < 71; i++) {
+   markup += `
+   <ul>
+   <li><span>Username</span>: yourname${i}</li>
+   <li><span>Maintenance/Repair</span>: repair${i}</li>
+   <li><span>Description</span>: fix problem${i}</li>
+   <li><span>Date</span>: ${i}/${i}/197${i}</li>
+   <li><span>(Dis)approve</span>: ${true ? "approved":"disapproved"}</li>
+   </ul>
+   `; 
+}
+if(document.querySelector(".content-area-approve"))
+document.querySelector(".content-area-approve").innerHTML += markup;
