193ea7acde326e92b6904aa15e8429e3639e981d
fattylee <fattylee.remod@gmail.com>
2018-05-20 12:06:33 +0100

    add dummyData/loggedinUsersRequest.js file to keep track of all user requests

--- controllers/userRequestController.js
+++ controllers/userRequestController.js
@@ -1,5 +1,8 @@
+const requests = require("./../dummyData/loggedinUsersRequest");
+
 const get = (req,res)=>{
-  res.send("List of logged in users");
+  //res.send("List of logged in users");
+  res.send(requests);
 };
 
 module.exports ={
