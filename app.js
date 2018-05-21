f3aa40a66b47327495834772ebc46d5b78ed9874
fattylee <fattylee.remod@gmail.com>
2018-05-21 20:55:03 +0100

    implement feedback on ui

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 app.listen(3000,()=>{
-  console.log("server listening for request on port", port);
+console.log("server listening for request on port", port);
 });
 
 app.use(bodyParser.json());
