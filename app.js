85050358c065054898c3ed4b389960da50a4c546
fattylee <fattylee.remod@gmail.com>
2018-05-21 21:03:01 +0100

    implement feedback on app.js file

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ import router from "./server/routes/userRoute";
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use("/users",router);
+
 const port = process.env.PORT || 3000;
 app.listen(3000,()=>{
 console.log("server listening for request on port", port);
 });
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use("/users",router);
