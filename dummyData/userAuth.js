2ad634bb5df1a18b0337e3409cb175dc64be1df5
fattylee <fattylee.remod@gmail.com>
2018-05-22 08:00:22 +0100

    create user authentication model

---
+++ dummyData/userAuth.js
@@ -0,0 +1,17 @@
+module.exports =[
+  {
+    id: 1,
+    name:"Balogun Fatai",
+    email: 'abcd@gmail.com',
+    username: 'yourname1',
+    password: '1234'
+  },
+  {
+    id: 2,
+    name:"Marcus Andela",
+    email: 'abcd@gmail.com',
+    username: 'yourname2',
+    password: '4321',
+  },
+  
+];
