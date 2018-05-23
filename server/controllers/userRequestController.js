814bb9d4f01691fb93b77ddc9e985fa608ac36a8
fattylee <fattylee.remod@gmail.com>
2018-05-23 08:39:11 +0100

    modify userRequestController.js

--- server/controllers/userRequestController.js
+++ server/controllers/userRequestController.js
@@ -1,57 +1,59 @@
-import requests from "./../../dummyData/loggedinUsersRequest";
+import requests from './../../dummyData/loggedinUsersRequest';
 
 
-class UserRequestHandler{
+class UserRequestHandler {
 
-  static getAllRequest(req,res){
+  static getAllRequest(req, res) {
     res.send(requests);
   }
 
-  static getARequest(req,res){
+  static getARequest(req, res) {
     const requestId = req.params.id;
     const request = requests.find(request => request.id === parseInt(requestId));
-  
-    if(!request){
+
+    if (!request) {
       res.status(404);
-      res.send("Invalid request id");
+      res.send('Invalid request id');
       return false;
     }
-    
+
     res.send(request);
   }
-  
-  static postARequest(req,res){
-    const { username, requestType, description } = req.body;
+
+  static postARequest(req, res) {
+    const { name,email, requestType, description } = req.body;
     const id = requests[requests.length - 1].id + 1;
     const newRequest = {
       id,
-      username,
+      name,
+      email,
       requestType,
       description,
     };
     requests.push(newRequest);
     res.status(201)
-    .json({
-      status: 'created',
-      message: 'Success'
-    })
+      .json({
+        status: 'created',
+        message: 'Success'
+      });
   }
 
-  static modifyRequest(req,res) {
+  static modifyRequest(req, res) {
     const request = requests.find(requestItem => requestItem.id === parseInt(req.params.id, 10));
     if (request) {
-      request.username = req.body.username;
+      request.name = req.body.name;
+      request.email = req.body.email;
       request.description = req.body.description;
       request.requestType = req.body.requestType;
       res.status(200)
-      .json({
-        request,
-        status: 'success',
-        message: 'modified successfully',
-      });
+        .json({
+          request,
+          status: 'success',
+          message: 'modified successfully',
+        });
     }
   }
-  
+
 
 }
 
