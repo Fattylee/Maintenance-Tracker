cdf590cff17000986f96d5e5782bf121a41a6e8e
fattylee <fattylee.remod@gmail.com>
2018-05-23 08:09:09 +0100

    add test.js file

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-import express from "express";
-import bodyParser from "body-parser";
-import router from "./server/routes/userRoute";
+import express from 'express';
+import bodyParser from 'body-parser';
+import router from './server/routes/userRoute';
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use("/api/v1/users",router);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api/v1/users', router);
 
 const port = process.env.PORT || 3000;
-app.listen(3000,()=>{
-console.log("server listening for request on port", port);
+app.listen(3000, () => {
+  console.log('server listening for request on port', port);
 });
