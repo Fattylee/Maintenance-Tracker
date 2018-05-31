3c1b65d0b15fc07a6d4dffc67eab2c9d9156a243
fattylee <fattylee.remod@gmail.com>
2018-05-31 19:42:05 +0100

    direct string

--- server/controllers/db.js
+++ server/controllers/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const Pool = pg.Pool;
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: 'postgres://qkfrkxrrdlqrse:87b0e33f56a055cd021b5cd1ba77cf4058f03ff7756524353df200a374f4b424@ec2-107-20-133-82.compute-1.amazonaws.com:5432/dd148ckbb7siqu',
   ssl: true
 });
 
