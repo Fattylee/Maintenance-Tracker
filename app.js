829ef79348b171b2bcd3a8068b4e7efb84ae7ccc
fattylee <fattylee.remod@gmail.com>
2018-06-13 09:16:36 +0100

    serve static page

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './server/routes/userRoute';
+import path from 'path';
 
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', express.static(path.resolve(__dirname, './ui/')));
 app.use('/api/v1', router);
 app.use(router);
 
