8510099aa168e78217ee04449a01aca4b29f698f
fattylee <fattylee.remod@gmail.com>
2018-06-19 09:01:29 +0100

    update db and improve admin css

--- server/controllers/db.js
+++ server/controllers/db.js
@@ -4,9 +4,9 @@ const Pool = pg.Pool;
 const pool = new Pool({
   connectionString: 
   
-  'postgresql://postgres:loocer212@@localhost:5432/maintenance-tracker',
-  // 'postgres://qkfrkxrrdlqrse:87b0e33f56a055cd021b5cd1ba77cf4058f03ff7756524353df200a374f4b424@ec2-107-20-133-82.compute-1.amazonaws.com:5432/dd148ckbb7siqu',
-  // ssl: false
+  // 'postgresql://postgres:loocer212@@localhost:5432/maintenance-tracker',
+  'postgres://qkfrkxrrdlqrse:87b0e33f56a055cd021b5cd1ba77cf4058f03ff7756524353df200a374f4b424@ec2-107-20-133-82.compute-1.amazonaws.com:5432/dd148ckbb7siqu',
+  ssl: true
 });
 
 export default pool;
