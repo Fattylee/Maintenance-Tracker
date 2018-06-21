a67f24ae2a192999c29854302f8f7fcee4a3a6df
fattylee <fattylee.remod@gmail.com>
2018-06-21 09:52:06 +0100

    improve admin dashboard ui

--- dummyData/userAuth.js
+++ dummyData/userAuth.js
@@ -1,22 +1,22 @@
-module.exports = [
+export default [
   {
     id: 1,
     name: 'Balogun Fatai',
-    email:"abcs@yahoo.com",
+    email: "abcs@yahoo.com",
     username: 'yourname1',
     password: '1234'
   },
   {
     id: 2,
     name: 'Marcus Andela',
-    email:"abc@gmail.com",
+    email: "abc@gmail.com",
     username: 'yourname2',
     password: '4321',
   },
   {
     id: 3,
     name: 'yourname3',
-    email:"abcds@yahoo.com",
+    email: "abcds@yahoo.com",
     username: 'yourname3',
     password: '394nd3',
   }
